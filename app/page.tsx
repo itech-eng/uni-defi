351c15c9ceef414c11588be9bca5175b937a7658
anirban_itech <anirban00537@gmail.com>
2024-01-31 19:15:13 +0600

    worked on landing banner section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,25 @@
 "use client";
 
+import ImageComponent from "@/components/image/image-component";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="bg-gray-900">
-      <section className="relative">
-        <div className="relative z-10 max-w-screen-xl mx-auto px-4 py-28 md:px-8">
+    <div className="bg-gray-900 h-screen">
+      <div className="absolute inset-0 m-auto max-w-xs h-[357px] blur-[118px] sm:max-w-md md:max-w-lg glow-bg"></div>
+      <section className="relative pt-14 md:pt-24">
+        <div>
+          <ImageComponent
+            alt="landing banner"
+            src="/landing-banner.png"
+            placeholder="blur"
+            height={300}
+            width={500}
+            className="mx-auto opacity-70"
+          />
+        </div>
+
+        <div className="relative z-10 max-w-screen-xl mx-auto px-4  md:px-8">
           <div className="space-y-5 max-w-4xl mx-auto text-center">
             <h2 className="text-4xl text-white font-extrabold mx-auto md:text-5xl">
               Trade crypto and NFTs with confidence
@@ -14,7 +29,6 @@ export default function Home() {
             </p>
           </div>
         </div>
-        <div className="absolute inset-0 m-auto max-w-xs h-[357px] blur-[118px] sm:max-w-md md:max-w-lg glow-bg"></div>
       </section>
     </div>
   );
---
+++ components/image/image-component.tsx
@@ -0,0 +1,39 @@
+// components/ImageComponent.tsx
+
+import Image, { ImageProps } from "next/image";
+
+interface ImageComponentProps extends ImageProps {
+  objectFit?: string;
+  objectPosition?: string;
+}
+
+const ImageComponent: React.FC<ImageComponentProps> = ({
+  src,
+  alt,
+  width = 100,
+  height = 100,
+  objectFit,
+  objectPosition,
+  className,
+  style,
+  ...rest
+}) => {
+  return (
+    <Image
+      src={src}
+      alt={alt}
+      height={height}
+      width={width}
+      quality={100}
+      className={className}
+      style={style}
+      blurDataURL={"/placeholder.png"}
+      placeholder="blur"
+      objectFit={objectFit || "cover"}
+      objectPosition={objectPosition || "center"}
+      {...rest}
+    />
+  );
+};
+
+export default ImageComponent;
