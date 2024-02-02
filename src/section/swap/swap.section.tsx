5095d93b85658302d4ef0ba3d3dedebd3c91b83d
anirban_itech <anirban00537@gmail.com>
2024-02-02 15:24:54 +0600

    worked on input design

--- src/section/swap/swap.section.tsx
+++ src/section/swap/swap.section.tsx
@@ -33,15 +33,19 @@ const SwapSection = () => {
                     inputMode="decimal"
                     className="bg-transparent p-0 border-none text-white placeholder:text-gray-400 text-4xl placeholder:text-4xl py-7 font-medium focus:outline-none focus:border-none"
                     placeholder="0"
+                    autoComplete="off"
+                    autoCorrect="off"
+                    autoCapitalize="off"
+                    spellCheck="false"
                   />
-                  <div className="text-md font-semibold text-white bg-primary w-72  h-10 flex items-center justify-center  px-1 py-1 rounded-3xl">
+                  <div className="text-md cursor-pointer font-semibold text-white bg-primary w-72 h-10 flex items-center justify-center px-1 py-1 rounded-3xl">
                     Select Token
                     <ChevronDown className="ml-2" size={18} />
                   </div>
                 </div>
               </div>
             </div>
-            <div className="absolute -translate-x-1/2 border-[4px] border-slate-950 -translate-y-1/2 flex w-10 h-10 bg-slate-900 rounded-xl items-center justify-center left-1/2">
+            <div className="absolute -translate-x-1/2 border-[4px] border-slate-950 -translate-y-1/2 flex w-10 h-10 bg-slate-900 rounded-xl items-center justify-center left-1/2 hover:bg-slate-800 hover:border-slate-700 cursor-pointer">
               <ArrowDown className="mx-auto text-3xl text-white" size={15} />
             </div>
             <div className="grid w-full mt-2 items-center gap-4">
@@ -56,8 +60,12 @@ const SwapSection = () => {
                     inputMode="decimal"
                     className="bg-transparent p-0 border-none text-white placeholder:text-gray-400 text-4xl placeholder:text-4xl py-7 font-medium focus:outline-none focus:border-none"
                     placeholder="0"
+                    autoComplete="off"
+                    autoCorrect="off"
+                    autoCapitalize="off"
+                    spellCheck="false"
                   />
-                  <div className="text-xl font-semibold text-white bg-slate-950 w-40  h-10 flex items-center justify-center border border-slate-800 px-3 rounded-3xl hover:bg-slate-800 hover:border-slate-700">
+                  <div className="text-xl cursor-pointer font-semibold text-white bg-slate-950 w-40  h-10 flex items-center justify-center border border-slate-800 px-3 rounded-3xl hover:bg-slate-800 hover:border-slate-700">
                     <img
                       src={"/networks/ethereum.png"}
                       className="h-5  mr-3 shrink-0"
@@ -71,7 +79,7 @@ const SwapSection = () => {
           </form>
         </CardContent>
         <CardFooter className="flex justify-between">
-          <Button className="bg-[#7e22ce4a] text-primary py-7 text-xl font-semibold rounded-2xl w-full">
+          <Button className="bg-[#7e22ce4a] text-primary py-7 text-xl font-semibold rounded-2xl w-full hover:text-white hover:bg-primary hover:border-primary  ">
             Connect Wallet
           </Button>
         </CardFooter>
