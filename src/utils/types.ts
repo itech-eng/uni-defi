7196ac3615a1f25549aabc833b8084d9a3b96cb3
dip1059 <dipankarsaha1059@gmail.com>
2024-02-05 16:20:37 +0600

    added coins & network mapping data

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -2,7 +2,7 @@ import { Currency } from "@uniswap/sdk-core";
 
 export class ContractData {
   address: string;
-  abi: any;
+  // abi: any;
 }
 
 export class Contract {
@@ -13,10 +13,26 @@ export class Contract {
   swap_router: ContractData;
 }
 
+export class CoinBasic {
+  // is_native: boolean;
+  code: string;
+  name: string; 
+  icon: string;
+}
+
 export class NetworkData {
+  label: string;
   value: string;
   chain_id: number;
-  native_currency: Currency;
-  contract: Contract;
+  native_currency_code: string;
   icon: string;
+  contract?: Contract;
+  coin_or_token?: NetworkCoinData;
+}
+
+export class NetworkCoinData {
+  [coin_slug: string]: { 
+    net_info: Currency, 
+    basic: CoinBasic 
+  } 
 }
