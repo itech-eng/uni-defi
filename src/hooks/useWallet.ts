0aa77ed7881f2fb36c14c56316fdb8cf9adee482
dip1059 <dipankarsaha1059@gmail.com>
2024-02-08 19:00:24 +0600

    useWallet hook updated

--- src/hooks/useWallet.ts
+++ src/hooks/useWallet.ts
@@ -2,76 +2,120 @@ import { useState, useEffect } from "react";
 import { providers } from "ethers";
 import { formatEther } from "ethers/lib/utils";
 import { useDispatch } from "react-redux";
-import { setWallet } from "@/store/slice/wallet.slice";
+import {
+  setToInitialWallet,
+  setWallet,
+  walletInitialState,
+  walletSliceType,
+} from "@/store/slice/wallet.slice";
 import { CHIAN_SLUG_MAPPING, NETWORK_DATA } from "../utils/network-data";
 import { useToast } from "../components/ui/use-toast";
+import { getFromLocalStorage, setToLocalStorage } from "../lib/helpers";
+import { LOCAL_STORAGE_KEY } from "../utils/coreconstants";
 
 interface WalletHookReturnType {
-  wallet_address: string | null;
-  chain_id: number;
+  connect: () => Promise<boolean>;
+  disconnect: () => Promise<boolean>;
+  switchToNetwork: (chain_id: number) => Promise<boolean>;
   balance: string | null;
-  networkName: string | null;
 }
-const isNetworkSupported = (chainId: number): boolean => {
+
+interface ConnectInfo {
+  chainId: string;
+}
+
+interface ProviderRpcError extends Error {
+  message: string;
+  code: number;
+  data?: unknown;
+}
+
+interface ProviderMessage {
+  type: string;
+  data: unknown;
+}
+
+export const isNetworkSupported = (chainId: number): boolean => {
   const networkSlug = CHIAN_SLUG_MAPPING[chainId];
   return !!NETWORK_DATA[networkSlug];
 };
 
-export const WalletHook = (): WalletHookReturnType => {
-  const [wallet_address, setWalletAddress] = useState<string | null>(null);
-  const [chain_id, setChainID] = useState<number>(-1);
+export const useWallet = (): WalletHookReturnType => {
   const [balance, setBalance] = useState<string | null>(null);
-  const [networkName, setNetworkName] = useState<string | null>(null);
+
   const dispatch = useDispatch();
   const { toast } = useToast();
 
   const loadWalletData = async () => {
     try {
-      const address = await getAddress();
-      const { networkName } = await getNetworkInfo();
-      const chain = await getChainInfo();
+      let address = "";
+      let chain = null;
 
-      if (!isNetworkSupported(chain)) {
-        console.log("Network not supported");
-        toast({
-          title: `Network not supported`,
-          description: "Please switch to a supported network.",
-        });
-        return;
+      if (!isDisconnected()) {
+        address = await getAddress();
+        // const { networkName } = await getNetworkInfo();
+        chain = await getChainInfo();
+
+        if (!isNetworkSupported(chain)) {
+          disconnect();
+          console.log("Network not supported");
+          toast({
+            title: `Network not supported`,
+            description: "Please switch to a supported network.",
+          });
+          return;
+        }
+
+        const walletBalance = await getBalance(address);
+        setBalance(walletBalance);
       }
 
-      const walletBalance = await getBalance(address);
-      setWalletAddress(address?.toLowerCase() ?? null);
-      setChainID(chain);
-      setNetworkName(networkName);
-      setBalance(walletBalance);
-      dispatch(
-        setWallet({
-          chain_id: chain,
-          wallet_address: address?.toLowerCase() ?? null,
-          balance: walletBalance,
-          network_name: networkName,
-        }),
-      );
+      if (isDisconnected()) {
+        dispatch(setToInitialWallet());
+      } else {
+        dispatch(
+          setWallet<walletSliceType>({
+            chain_id: chain,
+            wallet_address: address?.toLowerCase() ?? "",
+          }),
+        );
+      }
     } catch (error) {
       console.error("Error loading wallet:", error);
     }
   };
 
   useEffect(() => {
+    const handleConnect = async (connectInfo: ConnectInfo) => {
+      console.log("onConnect: ", connectInfo);
+    };
+
+    const handleDisconnect = async (error: ProviderRpcError) => {
+      console.log("onDisconnect: ", error);
+      disconnect();
+    };
+
+    const handleMessage = async (message: ProviderMessage) => {
+      console.log("onMessage: ", message);
+    };
+
     const handleAccountsChanged = async (accounts: string[]) => {
-      if (!accounts[0]) return;
-      console.log("Wallet address changed:");
+      console.log("Wallet address changed: ", accounts);
+      if (!accounts[0]) {
+        disconnect();
+        return;
+      }
       loadWalletData();
     };
 
     const handleChainChanged = async (chainId: string) => {
       if (!chainId) return;
-      console.log("Wallet chain changed:");
-      window.ethereum.off("accountsChanged", handleAccountsChanged);
+      console.log("Wallet chain changed: ", chainId);
+      // window.ethereum.off("accountsChanged", handleAccountsChanged);
 
-      if (!isNetworkSupported(Number(chainId))) {
+      if (!isDisconnected() && !isNetworkSupported(Number(chainId))) {
         console.log("Network not supported");
+        disconnect();
         toast({
           title: `Network not supported`,
           description: "Please switch to a supported network.",
@@ -82,34 +126,99 @@ export const WalletHook = (): WalletHookReturnType => {
       onAccountsChanged(handleAccountsChanged);
     };
 
+    onConnect(handleConnect);
+    onDisconnect(handleDisconnect);
+    onMessage(handleMessage);
     onAccountsChanged(handleAccountsChanged);
     onChainChanged(handleChainChanged);
     loadWalletData();
 
     // Clean up subscriptions when component unmounts
     return () => {
-      window.ethereum.off("accountsChanged", handleAccountsChanged);
-      window.ethereum.off("chainChanged", handleChainChanged);
+      // window.ethereum.off("accountsChanged", handleAccountsChanged);
+      // window.ethereum.off("chainChanged", handleChainChanged);
     };
   }, []);
 
+  const connect = async (
+    wallet: "metamask" | "coinbase" = "metamask",
+  ): Promise<boolean> => {
+    try {
+      if (wallet == "metamask" && !isMetaMaskInstalled()) {
+        toast({
+          title: `Metamask not installed`,
+          description: "Please install metamask wallet browser extension.",
+        });
+        throw new Error("Metamask not installed");
+      }
+
+      setToLocalStorage(LOCAL_STORAGE_KEY.IS_DISCONNECTED, "0");
+      window.ethereum.request({ method: "eth_requestAccounts" });
+      loadWalletData();
+
+      return true;
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  };
+
+  const disconnect = async (): Promise<boolean> => {
+    try {
+      setToLocalStorage(LOCAL_STORAGE_KEY.IS_DISCONNECTED, "1");
+      loadWalletData();
+      return true;
+    } catch (e) {
+      console.error("Error disconnecting Wallet:", e);
+      return false;
+    }
+  };
+
+  const switchToNetwork = async (chain_id: number): Promise<boolean> => {
+    if (!isMetaMaskInstalled()) return false;
+    try {
+      setToLocalStorage(LOCAL_STORAGE_KEY.IS_DISCONNECTED, "0");
+      chain_id &&
+        (await window.ethereum.request({
+          method: "wallet_switchEthereumChain",
+          params: [
+            {
+              chainId: `0x${chain_id.toString(16)}`,
+            },
+          ],
+        }));
+      loadWalletData();
+      return true;
+    } catch (e) {
+      console.error(e);
+      return false;
+    }
+  };
+
   return {
-    wallet_address,
-    chain_id,
+    connect,
+    disconnect,
+    switchToNetwork,
     balance,
-    networkName,
   };
 };
+
 export function ellipseAddress(
   address: string = "",
-  width: number = 10,
+  width: number = 7,
 ): string {
   return `${address.slice(0, width)}...${address.slice(-width)}`;
 }
+
 const isMetaMaskInstalled = (): boolean => {
   if (typeof window === "undefined") return false;
   const { ethereum } = window;
-  return Boolean(ethereum && ethereum.isMetaMask);
+  const is_installed = Boolean(ethereum && ethereum.isMetaMask);
+  // if (!is_installed) {
+  //   const msg = 'Metamask is not installed';
+  //   console.error(msg);
+  // }
+  return is_installed;
 };
 
 const getProvider = (): providers.Web3Provider | null => {
@@ -121,6 +230,21 @@ const getChainInfo = async (): Promise<number> => {
   const provider = getProvider();
   if (!provider) return -1;
   return (await provider.getNetwork()).chainId;
+};
+
+const onConnect = (callback: (connectInfo: ConnectInfo) => void): void => {
+  if (!isMetaMaskInstalled()) return;
+  window.ethereum.on("connect", callback);
+};
+
+const onDisconnect = (callback: (error: ProviderRpcError) => void): void => {
+  if (!isMetaMaskInstalled()) return;
+  window.ethereum.on("disconnect", callback);
+};
+
+const onMessage = (callback: (message: ProviderMessage) => void): void => {
+  if (!isMetaMaskInstalled()) return;
+  window.ethereum.on("disconnect", callback);
 };
 
 const onAccountsChanged = (callback: (address: string[]) => void): void => {
@@ -157,65 +281,58 @@ const getNetworkInfo = async (): Promise<{ networkName: string | null }> => {
   }
 };
 
-export const getBalance = async (address: string): Promise<string | null> => {
+export const getBalance = async (
+  address: string,
+  to_fixed = 5,
+): Promise<string | null> => {
   const provider = getProvider();
-  if (!provider) return null;
+  if (!address || !provider) return null;
   try {
     const balance = await provider.getBalance(address);
-    const etherBalance = formatEther(balance);
-    return etherBalance;
+    let nativeBalance = formatEther(balance);
+    nativeBalance = Math.abs(
+      Number(Number(nativeBalance).toFixed(to_fixed)),
+    ).toString();
+    return nativeBalance;
   } catch (e) {
     console.error("Error fetching balance:", e);
     return null;
   }
 };
 
-export const disconnectMetamask = async (): Promise<boolean> => {
-  if (!isMetaMaskInstalled()) return false;
-  try {
-    await window.ethereum.request({
-      method: "wallet_requestPermissions",
-      params: [
-        {
-          eth_accounts: {},
-        },
-      ],
-    });
-    return true;
-  } catch (e) {
-    console.error("Error disconnecting MetaMask:", e);
-    return false;
-  }
+export const isDisconnected = (): boolean => {
+  return !!Number(getFromLocalStorage(LOCAL_STORAGE_KEY.IS_DISCONNECTED));
 };
 
-export const connectMetamask = async (): Promise<boolean> => {
-  if (!isMetaMaskInstalled()) return false;
-  try {
-    await window.ethereum.request({ method: "eth_requestAccounts" });
-    return true;
-  } catch (e) {
-    console.log(e);
-    return false;
-  }
-};
+// export const disconnectMetamask = async (): Promise<boolean> => {
+//   if (!isMetaMaskInstalled()) return false;
+//   try {
+//     await window.ethereum.request({
+//       method: "wallet_requestPermissions",
+//       params: [
+//         {
+//           eth_accounts: {},
+//         },
+//       ],
+//     });
+//     return true;
+//   } catch (e) {
+//     console.error("Error disconnecting MetaMask:", e);
+//     return false;
+//   }
+// };
 
-export const switchToMainnet = async (): Promise<boolean> => {
-  if (!isMetaMaskInstalled()) return false;
-  try {
-    await window.ethereum.request({
-      method: "wallet_switchEthereumChain",
-      params: [
-        {
-          chainId: `0x${parseInt(process.env.NEXT_PUBLIC_APP_CHAIN).toString(16)}`,
-        },
-      ],
-    });
-    return true;
-  } catch (e) {
-    console.log(e);
-    return false;
-  }
-};
+// export const connectMetamask = async (): Promise<boolean> => {
+//   if (!isMetaMaskInstalled()) return false;
+//   try {
+//     setToLocalStorage(LOCAL_STORAGE_KEY.IS_DISCONNECTED, '0');
+//     await window.ethereum.request({ method: "eth_requestAccounts" });
+//     return true;
+//   } catch (e) {
+//     console.error(e);
+//     return false;
+//   }
+// };
 
 export const watchTransaction = (
   txHash: string,
