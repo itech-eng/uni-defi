92a5ed91df132d4a4641cdc2011889f595e36297
dip1059 <dipankarsaha1059@gmail.com>
2024-02-16 13:10:26 +0600

    testing files updated

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,8 @@
 const { getUncollectedFees } = require("./uncollected_fees");
-const { getTokenAmountsTheirs, getTokenAmountsOurs } = require("./liquidity_amount");
+const {
+  getTokenAmountsTheirs,
+  getTokenAmountsOurs,
+} = require("./liquidity_amount");
 
 // liquidity token amount
 const currentTick = -108166; // slot0 tick
@@ -12,22 +15,19 @@ const sqrtPriceX96 = Number(354994108308571189386340633n);
 // console.log('=======');
 // getTokenAmountsTheirs(liquidity, sqrtPriceX96, tickLower, tickUpper, 18, 18);
 
-
-
-
 // uncollected fee
-const { Token } = require('@uniswap/sdk-core');
+const { Token } = require("@uniswap/sdk-core");
 
 const SupportedChainId = {
   GOERLI: 5,
   SEPOLIA: 11155111,
 };
 
-const POSITION_CONTRACT_ADDRESS = '0x1238536071E1c677A632429e3655c799b22cDA52';
+const POSITION_CONTRACT_ADDRESS = "0x1238536071E1c677A632429e3655c799b22cDA52";
 
 const POOL_FACTORY_ADDRESS =
   // '0x1F98431c8aD98523631AE4a59f267346ea31F984'
-  '0x0227628f3F023bb0B980b67D528571c95c6DaC1c' //sepolia
+  "0x0227628f3F023bb0B980b67D528571c95c6DaC1c"; //sepolia
 
 const WETH_CONTRACT_ADDRESS =
   // '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
@@ -51,12 +51,19 @@ const DKFT20_TOKEN = new Token(
   "DK Free Token",
 );
 
-const { FeeAmount } = require('@uniswap/v3-sdk');
-const wallet_address = '0x9908CbCb070d1ed8d8f2c064b281D3029545b185';
-const nft_token_id = 7879;
+const { FeeAmount } = require("@uniswap/v3-sdk");
+const wallet_address = "0x9908CbCb070d1ed8d8f2c064b281D3029545b185";
+const nft_token_id = 7976;
 // const wallet_address = '0x31d4Ae75Cd68f3eaEfAa8b77fF1bc505FBC32184';
 // const nft_token_id = 7884;
 
-getUncollectedFees(DKFT20_TOKEN, WETH_TOKEN, FeeAmount.HIGH, 
-  POOL_FACTORY_ADDRESS, POSITION_CONTRACT_ADDRESS,
-  nft_token_id, wallet_address, wallet_address);
+getUncollectedFees(
+  DKFT20_TOKEN,
+  WETH_TOKEN,
+  FeeAmount.MEDIUM,
+  POOL_FACTORY_ADDRESS,
+  POSITION_CONTRACT_ADDRESS,
+  nft_token_id,
+  wallet_address,
+  wallet_address,
+);
--- tests/uncollected_fees.js
+++ tests/uncollected_fees.js
@@ -2,27 +2,27 @@ const { Pool } = require("@uniswap/v3-sdk");
 const { Position } = require("@uniswap/v3-sdk");
 const { ethers } = require("ethers");
 const { BigNumber } = require("@ethersproject/bignumber");
-const IUniswapV3PoolABI = require('@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json');
-const NonfungiblePositionManagerABI = require('@uniswap/v3-periphery/artifacts/contracts/interfaces/INonfungiblePositionManager.sol/INonfungiblePositionManager.json');
-const { computePoolAddress } = require('@uniswap/v3-sdk');
-const { noExponents } = require('./corefunctions');
+const IUniswapV3PoolABI = require("@uniswap/v3-core/artifacts/contracts/interfaces/IUniswapV3Pool.sol/IUniswapV3Pool.json");
+const NonfungiblePositionManagerABI = require("@uniswap/v3-periphery/artifacts/contracts/interfaces/INonfungiblePositionManager.sol/INonfungiblePositionManager.json");
+const { computePoolAddress } = require("@uniswap/v3-sdk");
+const { noExponents } = require("./corefunctions");
 
 const provider = new ethers.providers.JsonRpcProvider(
   "https://eth-sepolia.public.blastapi.io",
 );
 
 async function getUncollectedFees(
-  token0_obj, 
-  token1_obj, 
+  token0_obj,
+  token1_obj,
   pool_fee,
   pool_factory_address,
   position_contract_address,
   nft_token_id,
-  owner, 
+  owner,
   recipient,
 ) {
   const MAX_UINT128 = BigNumber.from(2).pow(128).sub(1);
-  
+
   const positionContract = new ethers.Contract(
     position_contract_address,
     NonfungiblePositionManagerABI.abi,
@@ -30,32 +30,37 @@ async function getUncollectedFees(
   );
   // console.log('positionContract: ', positionContract.getFunction('collect'), '\n');
 
-  // const poolInfo = await getPoolInfo(token0_obj, token1_obj, pool_fee, pool_factory_address);
+  const poolInfo = await getPoolInfo(
+    token0_obj,
+    token1_obj,
+    pool_fee,
+    pool_factory_address,
+  );
   // console.log('poolInfo: ', poolInfo, '\n');
 
-  // // construct pool instance
-  // const pool = new Pool(
-  //   token0_obj,
-  //   token1_obj,
-  //   Number(poolInfo.fee),
-  //   poolInfo.sqrtPriceX96.toString(),
-  //   poolInfo.liquidity.toString(),
-  //   Number(poolInfo.tick)
-  // )
+  // construct pool instance
+  const pool = new Pool(
+    token0_obj,
+    token1_obj,
+    Number(poolInfo.fee),
+    poolInfo.sqrtPriceX96.toString(),
+    poolInfo.liquidity.toString(),
+    Number(poolInfo.tick),
+  );
 
-  // const positionInfo = await positionContract.positions(nft_token_id);
+  const positionInfo = await positionContract.positions(nft_token_id);
   // console.log('positionInfo: ', positionInfo, '\n');
 
-  // const position = new Position({
-  //   pool: pool,
-  //   liquidity: positionInfo.liquidity.toString(),
-  //   tickLower: Number(positionInfo.tickLower),
-  //   tickUpper: Number(positionInfo.tickUpper),
-  // });
+  const position = new Position({
+    pool: pool,
+    liquidity: positionInfo.liquidity.toString(),
+    tickLower: Number(positionInfo.tickLower),
+    tickUpper: Number(positionInfo.tickUpper),
+  });
 
   // liquidity token amount
-  // console.log("amount0:", position.amount0.toSignificant(4));
-  // console.log("amount1:", position.amount1.toSignificant(4));
+  console.log("amount0:", position.amount0.toSignificant(6));
+  console.log("amount1:", position.amount1.toSignificant(6));
 
   /* GET ACCRUED UNCLAIMDED FEES */
   // callStatic simulates a call without state changes
@@ -71,16 +76,27 @@ async function getUncollectedFees(
   // console.log("Fee0: ", parseFloat(results.amount0) / 100);
   // console.log("Fee1: ", parseFloat(results.amount1) / 100);
 
-  const fee_token0 = (results.amount0 / 10 ** Number(token0_obj.decimals)).toFixed(Number(token0_obj.decimals));
-  const fee_token1 = (results.amount1 / 10 ** Number(token1_obj.decimals)).toFixed(Number(token1_obj.decimals));
+  const fee_token0 = (
+    results.amount0 /
+    10 ** Number(token0_obj.decimals)
+  ).toFixed(Number(token0_obj.decimals));
+  const fee_token1 = (
+    results.amount1 /
+    10 ** Number(token1_obj.decimals)
+  ).toFixed(Number(token1_obj.decimals));
 
   console.log("fee_token0: ", fee_token0.toString());
   console.log("fee_token1: ", fee_token1.toString());
 }
 
-async function getPoolInfo(token0_obj, token1_obj, pool_fee, pool_factory_address) {
+async function getPoolInfo(
+  token0_obj,
+  token1_obj,
+  pool_fee,
+  pool_factory_address,
+) {
   if (!provider) {
-    throw new Error('No provider')
+    throw new Error("No provider");
   }
 
   const currentPoolAddress = computePoolAddress({
@@ -88,13 +104,13 @@ async function getPoolInfo(token0_obj, token1_obj, pool_fee, pool_factory_addres
     tokenA: token0_obj,
     tokenB: token1_obj,
     fee: pool_fee,
-  })
+  });
 
   const poolContract = new ethers.Contract(
     currentPoolAddress,
     IUniswapV3PoolABI.abi,
-    provider
-  )
+    provider,
+  );
 
   const [token0, token1, fee, tickSpacing, liquidity, slot0] =
     await Promise.all([
@@ -104,7 +120,7 @@ async function getPoolInfo(token0_obj, token1_obj, pool_fee, pool_factory_addres
       poolContract.tickSpacing(),
       poolContract.liquidity(),
       poolContract.slot0(),
-    ])
+    ]);
 
   return {
     token0,
@@ -114,7 +130,7 @@ async function getPoolInfo(token0_obj, token1_obj, pool_fee, pool_factory_addres
     liquidity,
     sqrtPriceX96: slot0[0],
     tick: slot0[1],
-  }
+  };
 }
 
 module.exports = { getUncollectedFees };
