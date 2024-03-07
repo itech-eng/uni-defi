8d969576f998c723965dd6abb38aa2efd8587ce6
skalmurad <skmurad85@gmail.com>
2024-03-07 14:38:27 +0600

    added show hide closed position fn

--- src/section/pool/poolList.section.tsx
+++ src/section/pool/poolList.section.tsx
@@ -12,15 +12,19 @@ import { useSelector } from "react-redux";
 const PoolListSection = () => {
   const [positions, setPositions] = useState<PositionInfo[]>([]);
   const [loading, setLoading] = useState(true);
-  const { wallet_address: walletAddress, chain_id } = useSelector(
-    (state: IRootState) => state.wallet,
-  );
-  const handlePositionList = async () => {
+  const [showClosed, setShowClosed] = useState(false);
+
+  const {
+    wallet_address: walletAddress,
+    chain_id,
+    block_number,
+  } = useSelector((state: IRootState) => state.wallet);
+  const handlePositionList = async (load = true) => {
     try {
-      setLoading(true);
+      if (load) setLoading(true);
       const positions = walletAddress ? await getPositions() : [];
       setPositions(positions);
-      setLoading(false);
+      if (load) setLoading(false);
     } catch (error) {
       setLoading(false);
     }
@@ -28,6 +32,15 @@ const PoolListSection = () => {
   useEffect(() => {
     handlePositionList();
   }, [walletAddress, chain_id]);
+
+  useEffect(() => {
+    handlePositionList(false);
+  }, [block_number]);
+
+  const toggleClosedShowHide = () => {
+    setShowClosed((prev) => !prev);
+  };
+
   return (
     <div className="container text-white mt-36 px-72">
       <div className="flex justify-between items-center mb-6">
@@ -44,73 +57,83 @@ const PoolListSection = () => {
         </div>
       ) : (
         <div className="border border-slate-800 rounded-lg py-4  flex flex-col">
-          <div className="mb-4 border-b border-slate-800 pb-4 px-4">
+          <div className="flex justify-between mb-4 border-b border-slate-800 pb-4 px-4">
             <h2 className="text-xs ">Your positions ({positions.length})</h2>
+            <h2
+              className="text-xs underline cursor-pointer"
+              onClick={toggleClosedShowHide}
+            >
+              {showClosed ? "Hide Closed" : "Show Closed"}
+            </h2>
           </div>
-          {positions.map((position, idx) => (
-            <Link href={`/pool/${position.tokenId}`}>
-              <div
-                key={idx}
-                className="flex rounded-lg py-5 flex-row items-center justify-between px-4"
-              >
-                <div className="">
-                  <div className="flex items-center mb-2">
-                    <div className="relative">
-                      <img
-                        src={COIN_BAISC_DATA[position.token0.symbol].icon}
-                        className="h-7 w-7 ml-[20] rounded-full"
-                        alt=""
-                      />
-                      <img
-                        src={COIN_BAISC_DATA[position.token1.symbol].icon}
-                        className="h-7 w-7 top-0 absolute left-2  rounded-full"
-                        alt=""
-                      />
+          {positions.map((position, idx) =>
+            !position.closed || showClosed ? (
+              <Link href={`/pool/${position.tokenId}`}>
+                <div
+                  key={idx}
+                  className="flex rounded-lg py-5 flex-row items-center justify-between px-4"
+                >
+                  <div className="">
+                    <div className="flex items-center mb-2">
+                      <div className="relative">
+                        <img
+                          src={COIN_BAISC_DATA[position.token0.symbol].icon}
+                          className="h-7 w-7 ml-[20] rounded-full"
+                          alt=""
+                        />
+                        <img
+                          src={COIN_BAISC_DATA[position.token1.symbol].icon}
+                          className="h-7 w-7 top-0 absolute left-2  rounded-full"
+                          alt=""
+                        />
+                      </div>
+                      <div className="flex items-center gap-2 ml-5">
+                        <h3 className="text-[16px] font-medium text-white">
+                          {position.token0.symbol} / {position.token1.symbol}
+                        </h3>
+                        <span className="text-sm text-gray-400">
+                          {PoolFeeText[position.fee]}%
+                        </span>
+                      </div>
                     </div>
-                    <div className="flex items-center gap-2 ml-5">
-                      <h3 className="text-[16px] font-medium text-white">
-                        {position.token0.symbol} / {position.token1.symbol}
-                      </h3>
-                      <span className="text-sm text-gray-400">
-                        {PoolFeeText[position.fee]}%
-                      </span>
-                    </div>
-                  </div>
 
-                  <div className="text-[14px]">
-                    <span className=" mr-2 text-gray-500">
-                      Min:{" "}
-                      <span className="text-white">
-                        {formatAmountKnL(position.minPrice)}{" "}
-                        {position.token1.symbol} per {position.token0.symbol}
+                    <div className="text-[14px]">
+                      <span className=" mr-2 text-gray-500">
+                        Min:{" "}
+                        <span className="text-white">
+                          {formatAmountKnL(position.minPrice)}{" "}
+                          {position.token1.symbol} per {position.token0.symbol}
+                        </span>
                       </span>
-                    </span>
-                    ... {"  "}
-                    <span className=" text-gray-500 ">
-                      Max:{" "}
-                      <span className="text-white">
-                        {position.maxPrice != INFINITY_TEXT
-                          ? formatAmountKnL(position.maxPrice)
-                          : position.maxPrice}{" "}
-                        {position.token1.symbol} per {position.token0.symbol}
+                      ... {"  "}
+                      <span className=" text-gray-500 ">
+                        Max:{" "}
+                        <span className="text-white">
+                          {position.maxPrice != INFINITY_TEXT
+                            ? formatAmountKnL(position.maxPrice)
+                            : position.maxPrice}{" "}
+                          {position.token1.symbol} per {position.token0.symbol}
+                        </span>
                       </span>
+                    </div>
+                  </div>
+                  <div>
+                    <span className="font-medium text-[14px] text-gray-400 mr-2">
+                      {position.closed ? (
+                        <span className="text-grey-500">Closed</span>
+                      ) : position.inRange ? (
+                        <span className="text-green-500">In Range</span>
+                      ) : (
+                        <span className="text-yellow-500">Out of Range</span>
+                      )}
                     </span>
                   </div>
                 </div>
-                <div>
-                  <span className="font-medium text-[14px] text-gray-400 mr-2">
-                    {position.closed ? (
-                      <span className="text-grey-500">Closed</span>
-                    ) : position.inRange ? (
-                      <span className="text-green-500">In Range</span>
-                    ) : (
-                      <span className="text-yellow-500">Out of Range</span>
-                    )}
-                  </span>
-                </div>
-              </div>
-            </Link>
-          ))}
+              </Link>
+            ) : (
+              ""
+            ),
+          )}
           {positions.length === 0 && (
             <div className="text-center mt-4 text-gray-500">
               <Rows3 className="h-20 w-20 mx-auto" />
